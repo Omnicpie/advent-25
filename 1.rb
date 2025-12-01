ae4451c5eb35173b039a7fe3580e8d643088907a
Ry Anderson <60347977+Omnicpie@users.noreply.github.com>
2025-12-01 19:25:13 +0000

    Make it work for pt1

--- 1.rb
+++ 1.rb
@@ -1,16 +1,21 @@
 start_val = 50
-rotations = File.read("1.txt").split
+rotations = File.read("1-1.txt").split
 num_zeros = 0
+dial_size = 100
 
 
 rotations.each do |line|
   direction = line[0]
   amount_string = line[1, 2000]
   amount = amount_string.to_i
-  start_val = direction === "L" ? start_val - amount : start_val + amount
+  
+  distance = amount.remainder(dial_size)
+  distance_dir = (direction === "L" ? -1 : 1) * distance;
+  
+  start_val = start_val + distance_dir
 
-  if(start_val > 99) 
-    start_val = start_val - 100
+  if(start_val > 99)
+    start_val = start_val.remainder(100).abs
   end
 
   if(start_val < 0)
