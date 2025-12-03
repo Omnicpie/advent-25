b2fdb0d335a9e4929b904e970ddf5f45245bed56
Ry Anderson <60347977+Omnicpie@users.noreply.github.com>
2025-12-03 20:01:09 +0000

    Day 3 part 1 working, Fixes error with too much splitting

--- 3/3.rb
+++ 3/3.rb
@@ -1,15 +1,19 @@
 require 'benchmark'
 
-lines = File.read("preview.txt").split
+def part_1
+  lines = File.read("input.txt").split
 
-total = 0
-lines.each do |line|
-  numbers = line.chars.map{ |str| str.to_i }
-  max = numbers.first(numbers.size - 1).max
-  rest = line.split(max.to_s).last.chars.map{ |str| str.to_i }
-  max_2 = rest.max
-  num = "#{max}#{max_2}"
-  total += num.to_i
+  total = 0
+  lines.each do |line|
+    numbers = line.chars.map{ |str| str.to_i }
+    max = numbers.first(numbers.size - 1).max
+    rest = line.split(max.to_s, 2).last.chars.map{ |str| str.to_i }
+    max_2 = rest.max
+    num = "#{max}#{max_2}"
+    total += num.to_i
+  end
+
+  total
 end
 
-puts total
+puts Benchmark.measure { puts part_1 }
